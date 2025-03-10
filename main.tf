e5085a3935485831d9e2f93b2f12045daeb88726
santhosh <santhosh.ss7878@gmail.com>
2025-03-10 22:49:39 +0530

    push the files to azure repo

---
+++ main.tf
@@ -0,0 +1,12 @@
+#Resource Group creating for WebApp
+resource "azurerm_resource_group" "Webapprg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+#Azure WebApp creation
+resource "azurerm_static_web_app" "WebApp" {
+    name                = var.webapp_name
+    location            = var.location
+    resource_group_name = azurerm_resource_group.Webapprg.name
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+    description = "value for resource group"
+    type = string  
+}
+
+variable "location" {
+    description = "value for location"
+    type = string
+}
+
+variable "webapp_name" {
+    description = "value for webapp name"
+    type = string
+}
